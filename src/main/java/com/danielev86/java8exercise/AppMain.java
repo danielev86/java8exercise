e42b9be7de6460952d8d714b06371c21cf800eb4
danielev86 <danieleverr86@gmail.com>
2019-10-20 13:03:59 +0200

    Log person info in AppMain

--- src/main/java/com/danielev86/java8exercise/AppMain.java
+++ src/main/java/com/danielev86/java8exercise/AppMain.java
@@ -1,5 +1,7 @@
 package com.danielev86.java8exercise;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,11 +11,13 @@ import com.danielev86.java8exercise.service.impl.PersonServiceImpl;
 
 @ComponentScan(basePackages = "com.danielev86")
 public class AppMain {
+	
+	private static final Logger logger = LoggerFactory.getLogger(AppMain.class);
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = new AnnotationConfigApplicationContext(AppMain.class);
 		PersonService personService = ctx.getBean(PersonServiceImpl.class);
-		personService.getAllPersonsDetails().forEach(x -> System.out.println(x));
+		personService.getAllPersonsDetails().forEach(x -> logger.info(x.toString()));
 	}
 
 }
