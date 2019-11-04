383978d5d9ad7c92c045c387d62e7a4f5cf85b2f
danielev86 <danieleverr86@gmail.com>
2019-11-04 19:04:08 +0100

    Clean class

--- src/main/java/com/danielev86/java8exercise/AppMain.java
+++ src/main/java/com/danielev86/java8exercise/AppMain.java
@@ -6,8 +6,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
